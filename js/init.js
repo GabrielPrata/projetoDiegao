f05318eb2242aba2542828fe1f05efb3b0bfea45
GuilhermeZanetti <guilherme.t.zanetti@gmail.com>
2023-05-12 19:58:27 -0300

    wip: sobre

--- js/init.js
+++ js/init.js
@@ -1,4 +1,10 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav();
+$(document).ready(function () {
+  $(".sidenav").sidenav();
 
+  //#region Tabs do Sobre
+  var options = { swipeable: true, duration: 600 };
+  var el = document.querySelector("#tabSobre");
+  var instance = M.Tabs.init(el, options);
+  console.log(instance);
+  //#endregion
 });
