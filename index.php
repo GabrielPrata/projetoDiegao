5b2e95e3d517d2abb208b1815d6a700dfc709376
Gabriel <gabrielsprata@hotmail.com>
2023-05-11 22:14:27 -0300

    [wip] desenvolvimento pag Home

---
+++ index.php
@@ -0,0 +1,60 @@
+<!-- Página feita por Gabriel Prata -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bem Vindo! Poligrill</title>
+
+    <!-- Fontes -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@1,400&display=swap" rel="stylesheet">
+
+    <!-- Estilos -->
+    <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen, projection">
+
+    <!-- JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+    <script src="js/materialize.min.js" defer></script>
+    <script src="js/init.js"></script>
+</head>
+
+<body>
+    <?php
+    include 'navbar.html';
+    ?>
+    <section>
+        <div class="parallax-container valign-wrapper">
+            <div class="section no-pad-bot">
+                <div class="container">
+                    <div class="row center">
+                        <h5 class="tom-preto-text" id="texto-parallax-1">"Não fazemos churrasqueiras, transformamos vidas e propomos oportunidades de negócios"</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="parallax"><img src="img/carne-parallax.jpg" alt="Imagem Parallax 1"></div>
+        </div>
+    </section>
+    <br>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col s12 l4 right">
+                    <img src="" alt="">
+                </div>
+                <div class="col s12 l8 left">
+                    O ótimo resultado do seu negócio de espetinhos, que de algo aparentemente simples, o “assar espetinhos” precisou desenvolver melhorias para ser eficiente nos processos se deparando com diversos desafios.
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
