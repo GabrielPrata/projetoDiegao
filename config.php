542e80bc692ba79ae732593e1cb128bec08d80c4
Gabriel <gabrielsprata@hotmail.com>
2023-05-15 20:44:54 -0300

    [fix] Diretorios config

--- config.php
+++ config.php
@@ -1,12 +1,16 @@
 <?php
 define('APP_BASE_PATH', dirname(__FILE__));
 
-$dir = APP_BASE_PATH . 'htdocs';
+$dir =  getcwd();
 
+if ($dir == "projetoDiegao") {
+    $APP_IMAGE_PATH =  'img/';
+}else{
+    $APP_IMAGE_PATH = '../img/';
+}
 
 
 $APP_CSS_PATH = APP_BASE_PATH . '/css/';
-$APP_IMAGE_PATH = APP_BASE_PATH . '/img/';
 $APP_JAVASCRIPT_PATH = APP_BASE_PATH . '/js/';
 $APP_PAGES_PATH = APP_BASE_PATH . '/pages/';
 $APP_SHARED_PATH = APP_BASE_PATH . '/shared/';
