2ea7439236e7ed9a54c87d92062d0905acc81f9a
GuilhermeZanetti <guilherme.t.zanetti@gmail.com>
2023-05-16 22:14:38 -0300

    wip: sobre com carousel

--- js/init.js
+++ js/init.js
@@ -1,11 +1,6 @@
 $(document).ready(function () {
   $(".sidenav").sidenav();
   $(".parallax").parallax();
-
-  //#region Tabs do Sobre
-  var options = { swipeable: true, duration: 600 };
-  var el = document.querySelector("#tabSobre");
-  var instance = M.Tabs.init(el, options);
-  console.log(instance);
-  //#endregion
+  $(".carousel").carousel();
+  $("#tabSobre").tabs({ swipeable: true });
 });
--- pages/sobre.php
+++ pages/sobre.php
@@ -22,15 +22,47 @@
   ?>
   <div class="container">
     <div class="row">
+      <div class="s12">
+        <h2 class="">Sobre a PoliGrill</h2>
+      </div>
+      <div class="s12 m6">
+        <div class="carousel">
+          <a class="carousel-item" href="#estacionamentoAssadito01"><img
+              src="<?php echo $APP_IMAGE_PATH . '4-12-4Agregarnoestacionamentodonegcioexistente.png' ?>"></a>
+          <a class="carousel-item" href="#estacionamentoAssadito02"><img
+              src="<?php echo $APP_IMAGE_PATH . '4-12Agregarnoestacionamentodonegcioexistente.png' ?>"></a>
+          <a class="carousel-item" href="#Container"><img src="<?php echo $APP_IMAGE_PATH . 'CpiadePOST41.png' ?>"></a>
+          <a class="carousel-item" href="#Poligrill"><img
+              src="<?php echo $APP_IMAGE_PATH . 'WhatsAppImage2020-01-20at17.28.33.jpeg' ?>"></a>
+          <a class="carousel-item" href="#PoligrillImagemHome"><img src="<?php echo $APP_IMAGE_PATH . 'imagem-home1.png' ?>"></a>
+        </div>
+      </div>
+      <div class="s12 m4">
+        <h6 class=" paragrafo-sobre">
+          A PoliGrill, marca renomada de churrasqueiras, é uma empresa líder do setor, sediada em Limeira, São
+          Paulo. Situada em uma região reconhecida por sua excelência industrial, a PoliGrill pertence à OMZIPI, uma
+          empresa especializada em jóias, fábricas automotivas e maquinários para a indústria agro.
+        </h6>
+        <h6 class=" paragrafo-sobre">
+          Combinando a expertise em projetos industriais e a paixão pela culinária, a PoliGrill se esforça para
+          oferecer churrasqueiras que vão além das expectativas dos clientes. Nossa marca representa a fusão perfeita
+          entre design inovador, desempenho excepcional e uma experiência gastronômica única. Com a PoliGrill, você
+          terá a certeza de estar adquirindo um produto de qualidade superior, criado por uma equipe dedicada e
+          apaixonada por churrascos.
+        </h6>
+      </div>
+    </div>
+
+    <div class="row">
       <div class="col s12">
-        <ul class="tabs" id="tabSobre">
-          <li class="tab col s3">
+        <ul class="tabs tabs-fixed-width" id="tabSobre">
+          <li class="tab col s6 m4">
             <a class="active" href="#sobre-nos">Quem Somos?</a>
           </li>
-          <li class="tab col s3">
+          <li class="tab col s6 m4">
             <a href="#o-que-fazemos">O que fazemos?</a>
           </li>
-          <li class="tab col s3">
+          <li class="tab col s7 m4">
             <a href="#como-nascemos">Como nascemos?</a>
           </li>
         </ul>
@@ -44,13 +76,13 @@
               </div>
             </div>
           </div>
-          <div class="col s12 m4" style="min-height: 100% !important;">
-            <h6>
+          <div class="col s12 m6" style="min-height: 100% !important;">
+            <h6 class="center-align paragrafo-sobre">
               A PoliGrill, marca renomada de churrasqueiras, é uma empresa líder do setor, sediada em Limeira, São
               Paulo. Situada em uma região reconhecida por sua excelência industrial, a PoliGrill pertence à OMZIPI, uma
               empresa especializada em jóias, fábricas automotivas e maquinários para a indústria agro.
             </h6>
-            <h6>
+            <h6 class="center-align paragrafo-sobre">
               Combinando a expertise em projetos industriais e a paixão pela culinária, a PoliGrill se esforça para
               oferecer churrasqueiras que vão além das expectativas dos clientes. Nossa marca representa a fusão
               perfeita entre design inovador, desempenho excepcional e uma experiência gastronômica única. Com a
@@ -76,22 +108,18 @@
             </div>
           </div>
           <div class="col s12 m4">
-            <h6>
+            <h6 class="center-align paragrafo-sobre">
               Desfrute da extraordinária experiência dos espetinhos gourmet do "O ESPETINHO DA HORA", criados pelo
               renomado projetista de máquinas industriais, nosso CEO e fundador David Zanetti. Cada detalhe foi
               cuidadosamente aprimorado para garantir eficiência, sabor e uma experiência gastronômica única. Nossa
               equipe enfrentou desafios e encontrou soluções engenhosas, resultando em espetinhos cuidadosamente
-              montados, com ingredientes frescos e selecionados, proporcionando uma fusão perfeita de sabores. Junte-se
-              a nós nessa jornada sensorial e descubra um novo nível de prazer ao saborear nossos espetinhos gourmet.
+              montados, com ingredientes frescos e selecionados, proporcionando uma fusão perfeita de sabores.
             </h6>
-            <h6>
+            <h6 class="center-align paragrafo-sobre">
               Na PoliGrill, não apenas criamos as melhores churrasqueiras de espetinhos do mundo, mas também oferecemos
               uma experiência única de assar e saborear. Combinando paixão e expertise em projetos industriais, nossa
               marca é sinônimo de excelência e inovação. Nossas churrasqueiras foram projetadas para superar desafios
-              como desperdícios, limpeza e temperatura, proporcionando maior produtividade e satisfação. Desfrute de
-              momentos memoráveis ao lado de amigos e familiares, saboreando espetinhos cuidadosamente preparados,
-              criando uma fusão perfeita de sabores que vai encantar seu paladar. Junte-se à PoliGrill e descubra o
-              verdadeiro prazer de assar espetinhos.
+              como desperdícios, limpeza e temperatura, proporcionando maior produtividade e satisfação.
             </h6>
             </p>
           </div>
@@ -108,13 +136,13 @@
             </div>
           </div>
           <div class="col s12 m4">
-            <h6>
+            <h6 class="center-align paragrafo-sobre">
               A PoliGrill nasceu da necessidade de aprimorar e otimizar o processo de assar espetinhos, enfrentando
               desafios como a limpeza da churrasqueira, que demandava duas horas diárias. Movido pela busca por
               soluções, nosso CEO e projetista industrial, David Zanetti, embarcou em um projeto de melhorias para todas
               as etapas do processo de uma churrasqueira de espetinhos.
             </h6>
-            <h6>
+            <h6 class="center-align paragrafo-sobre">
               Após superar obstáculos como desperdícios, limpeza, temperatura externa e redução no consumo de carvão,
               nasceu a melhor churrasqueira de espetinhos do mundo. Essa inovação não apenas aumentou os lucros e a
               produtividade, mas também proporcionou maior satisfação aos clientes. Assim, a marca PoliGrill foi
