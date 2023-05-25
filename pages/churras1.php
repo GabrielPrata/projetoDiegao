b571dc9f4a5fd445665194097d29092e2ac21f95
Gabriel Higashi <gabriel.higashiii@gmail.com>
2023-05-25 16:28:07 -0300

    pag produtos
    ih rapaziada, tentei

---
+++ pages/churras1.php
@@ -0,0 +1,185 @@
+<!-- higashi -->
+<?php
+include '..\config.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="imagem/png" href="img/icons/favicon.ico" />
+    <title>Bem Vindo! Poligrill</title>
+
+    <!-- Fontes -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@1,400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/churras1.css">
+
+    <!-- Estilos -->
+    <link href="<?php echo $APP_CSS_PATH . 'materialize.css'; ?>" type="text/css" rel="stylesheet" media="screen,projection" />
+    <link rel="stylesheet" type="text/css" href="<?php echo $APP_CSS_PATH . 'style.css'; ?>" media="screen, projection">
+    <link rel="stylesheet" type="text/css" href="<?php echo $APP_CSS_PATH . 'home.css'; ?>" media="screen, projection">
+
+
+    <!-- JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+    <script src="<?php echo $APP_JAVASCRIPT_PATH .  'materialize.min.js'; ?>" defer></script>
+    <script src="<?php echo $APP_JAVASCRIPT_PATH . 'init.js'; ?> "></script>
+    <script src="<?php echo $APP_JAVASCRIPT_PATH . 'popUpWhats.js'; ?>"></script>
+    <script src="<?php echo $APP_JAVASCRIPT_PATH . 'animate.js'; ?>"></script>
+</head>
+
+<body>
+    <?php
+    include $APP_SHARED_PATH . 'navbar.php';
+    ?>
+
+    <section class="sec-prod">
+        <div class="row apre">
+            <div class="col s6">
+                <img src="../img/churras1a.png" class="primary">
+            </div>
+            <div class="col s2">
+                <img src="../img/churras1a.png" class="menu-img">
+                <img src="../img/churras1b.png" class="menu-img">
+                <img src="../img/churras1c.png" class="menu-img">
+                <img src="../img/churras1d.png" class="menu-img">
+            </div>
+            <div class="col s4">
+                <h3>Churrasqueira de Embutir Gourmet</h3>
+                <p>Inox a Carvão c/ 2 Grelha Argentina – PoliGrill 2G5EBIC</p>
+                <ul>
+                    <li><span class="ttlist">Linha</span>  Residencial</li>
+                    <li><span class="ttlist">Subcategoria:</span> Portátil</li>
+                    <li><span class="ttlist">Cód. Ref.:</span> 2G5IC</li>
+                    <li><span class="ttlist">Material:</span> Inox 430</li>
+                    <li><span class="ttlist">Assar com:</span> Carvão</li>
+                    <li><span class="ttlist">Voltagem:</span> N/A</li>
+                </ul>
+                <br>
+                <button class="btnorca">orçamento</button>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col s12 center-align firstwrite">
+                <span class="flow-text">Churrasqueira de Embutir Gourmet - Inox a Carvão c/ 2 Grelha Argentina</span>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col s4">
+                <div class="ltcards">
+                    <h4 class="tt-card center-align">RESIDENCIAL DE EMBUTIR</h4>
+                    <p class="txt-card">Saiba mais detalhes sobre nossaChurrasqueira de EmbutirGourmet - Inox a Carvão c/ 2Grelha Argentina. Aschurrasqueiras de embutirgourmet da PoliGrill trazem todaa praticidade já conhecida dosoutros modelos, de uma maneirasimples de embutir ela na suacasa. Aproveite o design elegantee moderno para usá-la emambientes externos ou internosno dia a dia em seu lar.</p>
+                </div>
+            </div>
+
+            <div class="col s4">
+                <div class="ltcards">
+                    <h4 class="tt-card center-align">PEÇAS DE ENCAIXE FÁCIL</h4>
+                    <p class="txt-card">As churrasqueiras de embutirgourmet da PoliGrill sãoproduzidas para termanutenção simples, umproduto de alta qualidade quedure por muitos anos. Amanutenção é simples, pode serfeita por você mesmo, já que aspeças são de encaixe fácil epodem ser adquiridas, casonecessário, no nosso site.Montar, desmontar, limpar econsertar uma churrasqueiraPoliGrill é muito simples.</p>
+                </div>
+            </div>
+
+            <div class="col s4">
+                <div class="ltcards">
+                    <h4 class="tt-card center-align title">ACESSÓRIOS DE PRIMEIRA</h4>
+                    <p class="txt-card">Também conhecidas comoparrilha, a grelha argentina tema função de escoar a gorduradas carnes para reservatórios,isso evita que caia no carvãoevitando o excesso de fumaça,além de labaredas e odesperdício de calor. A grelhaargentina permite assar suacarne mais próxima a brasa semo perigo de labaredas e fumaça,deixando-a mais suculentas,ideal para cortes de carne maisaltos. A gordura que descepelas canaletas pode seraplicada na carne com pincéisde silicone (não inclusos), porexemplo, deixando a maisbonita e suculenta.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col s12 center-align">
+                <h4 class="title">ECONÔMICA E AGRADÁVEL DE UTILIZAR</h4>
+                <p>Desenvolvemos um produto que permite o uso de maneira simples e agradável, e ao mesmo tempo consome menos carvão. O porta braseiro fica dentro da caixa externa, entre eles um bolsão de ar evita que o calor se espalhe pela área onde está a churrasqueira, permitindo um manuseio agradável e economizando carvão com a concentração de calor no braseiro.</p>
+                
+                <h4 class="title">GARANTIA DE 6 MESES </h4>
+                <p>Todos os nossos produtos tem 6 meses de garantia contra defeitos de fabricação. Oferecemos tranquilidade para que possa usufruir de toda a qualidade de nossos produtos.</p>
+            </div>
+        </div>
+
+        <div class="row center-align">
+            <p class="title">Nossos Clientes!</p>
+            <div class="col s12">
+                <div class="carousel">
+                    <a class="carousel-item" href="../img/cli1.png"><img src="../img/cli1.png"></a>
+                    <a class="carousel-item" href="../img/cli2.png"><img src="../img/cli2.png"></a>
+                    <a class="carousel-item" href="../img/cli3.png"><img src="../img/cli3.png"></a>
+                    <a class="carousel-item" href="../img/cli4.png"><img src="../img/cli4.png"></a>
+                    <a class="carousel-item" href="../img/cli5.png"><img src="../img/cli5.png"></a>
+                    <a class="carousel-item" href="../img/cli6.png"><img src="../img/cli6.png"></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col s12 center-align">
+                <p class="text-vid">A experiência do cliente é o combustível que impulsiona o nosso desejo de ser cada dia melhor</p>
+                <iframe width="800" height="500" src="https://www.youtube.com/embed/Ui5h13bIGmE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col s12 prod-rel">Produtos relacionados</div>
+        </div>
+
+        <div class="row">
+            <div class="col s4">
+                 <div class="card">
+                    <div class="card-image">
+                        <img src="../img/churras2.png">
+                        <span class="card-title">PoliGrill G5IC</span>
+                    </div>
+                    <div class="card-content">
+                        <p>Saiba mais detalhes sobre nossa Churrasqueira de Embutir Gourmet – Inox a Carvão c/ Grelha Argentina – Inox a Carvão c/ Grelha Argentina.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">Conferir!</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col s4">
+                 <div class="card">
+                    <div class="card-image">
+                        <img src="../img/churras3.png">
+                        <span class="card-title">PoliGrill 2G5IC</span>
+                    </div>
+                    <div class="card-content">
+                        <p>Churrasqueira Gourmet Inox a Carvão c/ Grelha Argentina, o modelo acompanha 2 Grelha Argentina.</p><br>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">Conferir!</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col s4">
+                 <div class="card">
+                    <div class="card-image">
+                        <img src="../img/churras4.png">
+                        <span class="card-title">PoliGrill M3G3IC</span>
+                    </div>
+                    <div class="card-content">
+                        <p>Saiba mais detalhes sobre nossa Churrasqueira Gourmet portátil Inox a Carvão c/ Grill Giratório + Grelha Argentina a Churrasqueira Gourmet PoliGrill.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">Conferir!</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <?php
+    include $APP_SHARED_PATH . 'footer.php';
+    ?>
+</body>
+
+</html>
